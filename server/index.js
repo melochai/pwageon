5376645d2159b8417f75b6349b94711a2305b98b
Chang Yea Moon <changyeamoon@gmail.com>
2019-03-04 23:30:24 -0800

    move index.html to dist, refactor proxy

--- server/index.js
+++ server/index.js
@@ -6,6 +6,14 @@ const app = express();
 
 const { SERVER, PORT } = process.env;
 
-app.use(express.static(path.resolve(__dirname, '../')));
+// app.use(express.static(path.resolve(__dirname, '../dist')));
+
+// app.get('/api', (req, res) => {
+//   res.json('yo');
+// });
+
+app.get('/anything', (req, res) => {
+  res.json('yoo');
+});
 
 app.listen(PORT, () => console.log(`Listening on ${SERVER}:${PORT}`));
