36b6240853f4a7c26db13e96b9869a4b54ab7223
howardNa <howardna317@gmail.com>
2019-03-06 14:49:43 -0800

    staging for push

--- client/components/Main.jsx
+++ client/components/Main.jsx
@@ -1,29 +1,31 @@
-import React from 'react';
-import { Link } from 'react-router-dom';
-import '../css/styles.css';
-
-
+import React from "react";
+import { Link } from "react-router-dom";
+import "../css/styles.css";
 
 // Components
-const Main = ({showCompose, displayPigi, incrementPigi}) => {
-    return (
-      <div>
-        <div className="header"></div>
-        <img className="signin" src={displayPigi.image} style={{width: '355px', align: 'center'}}/>
-        <button onClick={incrementPigi}>Next Pigeon</button>
-        <h1 className="signin">{displayPigi.name}</h1>
-        <div className="stats">
-          <p>Speed : {displayPigi.speed}</p>
-          <p>Stamina : {displayPigi.stamina}</p>
-          <p>Success : {displayPigi.success}</p>
-        </div>
-        <div>
-            <Link to="/compose" style={{ textDecoration: 'none' }}>
-              <button className="message">Prepare Message</button>
-            </Link>
-        </div>
+const Main = ({ showCompose, displayPigi, incrementPigi }) => {
+  return (
+    <div>
+      <div className="header" />
+      <img
+        className="signin"
+        src={displayPigi.image}
+        style={{ width: "355px", align: "center" }}
+      />
+      <button onClick={incrementPigi}>Next Pigeon</button>
+      <h1 className="signin">{displayPigi.name}</h1>
+      <div className="stats">
+        <p>Speed : {displayPigi.speed}</p>
+        <p>Stamina : {displayPigi.stamina}</p>
+        <p>Success : {displayPigi.success}</p>
       </div>
-    )       
+      <div>
+        <Link to="/compose" style={{ textDecoration: "none" }}>
+          <button className="message">Prepare Message</button>
+        </Link>
+      </div>
+    </div>
+  );
 };
 
 export default Main;
