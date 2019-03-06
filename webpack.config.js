b4621c6e15de3a5d97c1ccc27c63355d3cc127f9
howardNa <howardna317@gmail.com>
2019-03-06 11:57:24 -0800

    added test comp for react router

--- webpack.config.js
+++ webpack.config.js
@@ -7,7 +7,7 @@ module.exports = {
   entry: './client/index.js',
   output: {
     path: path.join(__dirname, '/dist'),
-    filename: 'bundle.js',
+    filename: 'bundle.js'
   },
   devServer: {
     contentBase: './dist',
