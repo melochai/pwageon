89d9a5def7be369e766151b35b7358a2083a6016
Chang Yea Moon <changyeamoon@gmail.com>
2019-03-07 13:52:43 -0800

    create functional google oauth button

--- client/components/Main.jsx
+++ client/components/Main.jsx
@@ -1,17 +1,13 @@
-import React from "react";
-import { Link } from "react-router-dom";
-import "../css/styles.css";
+import React from 'react';
+import { Link } from 'react-router-dom';
+import '../css/styles.css';
 
 // Components
 const Main = ({ showCompose, displayPigi, incrementPigi }) => {
   return (
     <div>
       <div className="header" />
-      <img
-        className="signin"
-        src={displayPigi.image}
-        style={{ width: "355px", align: "center" }}
-      />
+      <img className="signin" src={displayPigi.image} style={{ width: '355px', align: 'center' }} />
       <button onClick={incrementPigi}>Next Pigeon</button>
       <h1 className="signin">{displayPigi.name}</h1>
       <div className="stats">
@@ -20,7 +16,7 @@ const Main = ({ showCompose, displayPigi, incrementPigi }) => {
         <p>Success : {displayPigi.success}</p>
       </div>
       <div>
-        <Link to="/compose" style={{ textDecoration: "none" }}>
+        <Link to="/compose" style={{ textDecoration: 'none' }}>
           <button className="message">Prepare Message</button>
         </Link>
       </div>
--- webpack.config.js
+++ webpack.config.js
@@ -1,13 +1,24 @@
 const path = require('path');
 const { HotModuleReplacementPlugin } = require('webpack');
 const CopyWebpackPlugin = require('copy-webpack-plugin');
+const webpack = require('webpack');
+const dotenv = require('dotenv');
+
+// call dotenv and it will return an Object with a parsed key
+const env = dotenv.config().parsed;
+
+// reduce it to a nice object, the same as before
+const envKeys = Object.keys(env).reduce((prev, next) => {
+  prev[`process.env.${next}`] = JSON.stringify(env[next]);
+  return prev;
+}, {});
 
 module.exports = {
   mode: 'development',
   entry: './client/index.js',
   output: {
     path: path.join(__dirname, '/dist'),
-    filename: 'bundle.js'
+    filename: 'bundle.js',
   },
   devServer: {
     contentBase: './dist',
@@ -46,5 +57,6 @@ module.exports = {
   plugins: [
     new HotModuleReplacementPlugin(),
     new CopyWebpackPlugin([{ from: 'client/assets', to: 'assets' }]),
+    new webpack.DefinePlugin(envKeys),
   ],
 };
